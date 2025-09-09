e877724fb7aa2405763ba7d68f0b0bb25b383f45
Vladyslav Pihol <freepgvlad@gmail.com>
2025-09-09 12:14:40 +0300

    add to service/models model Cook

--- service/models.py
+++ service/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("name", )
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ("years_of_experience", )
+
+    def __str__(self):
+        return f"{self.username}: ({self.first_name} {self.last_name})"
