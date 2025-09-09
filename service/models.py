ca1f85fbf718cb939f13273646fc623a99adec97
Vladyslav Pihol <freepgvlad@gmail.com>
2025-09-09 12:38:39 +0300

    add to settings installed apps service and add to service/models model Dish or implementation migrations

--- service/models.py
+++ service/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -13,10 +14,36 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.PositiveIntegerField(default=0)
+    years_of_experience = models.PositiveIntegerField(
+        default=0
+    )
 
     class Meta:
         ordering = ("years_of_experience", )
 
     def __str__(self):
         return f"{self.username}: ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    ingredient = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="dishes"
+    )
+
+    def __str__(self):
+        return f"{self.name} (price: {self.price} , dish type: {self.dish_type.name})"
+
+    # def get_absolute_url(self):
+    #     return reverse("service:dish-detail", args=[str(self.id)])
